43aa2259ff2b135bda4cbe141d52740e223f3d86
joshgit <jschermann91@gmail.com>
2022-01-05 17:22:54 -0600

    comment routes

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const sequelize = require("../../config/connection");
-const { Post, User, Vote } = require("../../models");
+const { Post, User, Vote, Comment } = require("../../models");
 
 // Get all posts (api/posts/)
 router.get("/", (req, res) => {
@@ -17,8 +17,19 @@ router.get("/", (req, res) => {
         "vote_count",
       ],
     ],
+
     order: [["created_at", "DESC"]],
+
     include: [
+      // include the Comment model here:
+      {
+        model: Comment,
+        attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+        include: {
+          model: User,
+          attributes: ["username"],
+        },
+      },
       {
         model: User,
         attributes: ["username"],
@@ -51,6 +62,14 @@ router.get("/:id", (req, res) => {
       ],
     ],
     include: [
+      {
+        model: Comment,
+        attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+        include: {
+          model: User,
+          attributes: ["username"],
+        },
+      },
       {
         model: User,
         attributes: ["username"],
