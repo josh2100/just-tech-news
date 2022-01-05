fcce1cdebc2195432320125a0a6705682e392c07
joshgit <jschermann91@gmail.com>
2022-01-05 17:37:41 -0600

    connect to jawsdb

--- config/connection.js
+++ config/connection.js
@@ -4,15 +4,33 @@ const Sequelize = require("sequelize");
 require("dotenv").config();
 
 // create connection to our db
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PW,
-  {
-    host: "127.0.0.1", // localhost for normal people
-    dialect: "mysql",
-    port: 3306, // Connection to MySQL protocol
-  }
-);
+// const sequelize = new Sequelize(
+//   process.env.DB_NAME,
+//   process.env.DB_USER,
+//   process.env.DB_PW,
+//   {
+//     host: "127.0.0.1", // localhost for normal people
+//     dialect: "mysql",
+//     port: 3306, // Connection to MySQL protocol
+//   }
+// );
+
+let sequelize;
+// If hosted on Heroku with JawsDB
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  // Else configure for local computer
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PW,
+    {
+      host: "127.0.0.1",
+      dialect: "mysql",
+      port: 3306,
+    }
+  );
+}
 
 module.exports = sequelize;
