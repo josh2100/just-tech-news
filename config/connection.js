c6a15b21f24444dd04655ff7a0d479a9e4b0d125
joshgit <jschermann91@gmail.com>
2022-01-09 11:03:24 -0600

    login logout

--- config/connection.js
+++ config/connection.js
@@ -3,18 +3,6 @@ const Sequelize = require("sequelize");
 // To secure password
 require("dotenv").config();
 
-// create connection to our db
-// const sequelize = new Sequelize(
-//   process.env.DB_NAME,
-//   process.env.DB_USER,
-//   process.env.DB_PW,
-//   {
-//     host: "127.0.0.1", // localhost for normal people
-//     dialect: "mysql",
-//     port: 3306, // Connection to MySQL protocol
-//   }
-// );
-
 let sequelize;
 // If hosted on Heroku with JawsDB
 if (process.env.JAWSDB_URL) {
--- server.js
+++ server.js
@@ -1,10 +1,33 @@
 const express = require("express");
-const routes = require("./routes");
+const routes = require("./controllers");
 const sequelize = require("./config/connection");
 const path = require("path");
+const session = require("express-session");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+
+// Handlebars requirements
+const exphbs = require("express-handlebars");
+const hbs = exphbs.create({});
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+// Session requirements
+const sess = {
+  secret: "Super secret secret",
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
+
+app.use(session(sess));
+
+// Handlebars methods
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
